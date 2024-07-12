5e326287f9a389affb51eaae61b5965b414656d8
Ethan Burkett <82228572+EthanBurkett@users.noreply.github.com>
2024-07-12 10:35:34 -0500

    Update auto-mid.sh

--- auto-mid.sh
+++ auto-mid.sh
@@ -1,4 +1,5 @@
-rm /etc/machine-id -rf
-dbus-uuidgen --ensure
-systemd-machine-id-setup
+truncate -s 0 /etc/machine-id /var/lib/dbus-machine-id
+#rm /etc/machine-id -rf
+#dbus-uuidgen --ensure
+#systemd-machine-id-setup
 systemctl restart systemd-networkd
